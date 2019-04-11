c4561e23aa54d3d6011165db734ff1f9615fbff5
chanjeed <chanjeedtu75@gmail.com>
2019-04-11 20:51:55 +0900

    js

--- src/routes/web.php
+++ src/routes/web.php
@@ -48,7 +48,7 @@ Route::get('/home/profile/image', 'ImageController@index');
 Route::get('/liked/profile', 'LikesController@profile');
 Route::get('/home/liked', 'LikesController@likedusers');
 
-Routh::post('/like',function(){
+Route::post('/like',function(){
   if(Request::ajax()){
     return Response::json(Request::all());
   }
