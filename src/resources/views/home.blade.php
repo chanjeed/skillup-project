162ba06ee61a8fb7d476bdb982476f18a0ba7937
chanjeed <chanjeedtu75@gmail.com>
2019-04-12 02:29:38 +0900

    js

--- src/resources/views/home.blade.php
+++ src/resources/views/home.blade.php
@@ -156,7 +156,7 @@ for ($i = 0; $i < $end; $i++) {
         </form>
 
 
-        <button  width='75' height='75' position='relative' onclick="onClickBtn();" id="likeBtn" value="<?php echo htmlspecialchars($images[$i]->id);?>">
+        <button  width='75' height='75' position='relative' onclick="onClickBtn();" id="likeBtn" value=<?=$images[$i]->id)?>>
           like
         </button>
         <div id="postRequestData">
