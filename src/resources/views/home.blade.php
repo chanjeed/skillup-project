186a175bac2e9ffce1199da2caacffc1f305217f
chanjeed <chanjeedtu75@gmail.com>
2019-04-11 21:36:59 +0900

    js

--- src/resources/views/home.blade.php
+++ src/resources/views/home.blade.php
@@ -20,7 +20,7 @@
 @endempty
 
 
-<script src="{{ asset('/js/like.js') }}"></script>
+<script type="text/javascript" src="{{ asset('/js/like.js') }}"></script>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
 
 
